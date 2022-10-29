3f0206c35886927384117011378a43cd15e8afba
Renyi Tang <renyi.tang@yahoo.com>
2022-10-29 17:46:43 -0400

    fix s_t, add coco dataset, training

--- src/dataset.py
+++ src/dataset.py
@@ -1,11 +1,81 @@
+from collections import defaultdict
+import math
+
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, Sampler
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
 from random import randint
 import numpy as np
+import pickle
+from pathlib import Path
+
+
+class COCOCaptions(Dataset):
+    # split: train, dev, test
+    def __init__(self, datadir, split) -> None:
+        super().__init__()
+
+        # image data, n/5 * 3072 (=32*32*3)  each image has 5 captions
+        data_path = Path(datadir, f"{split}-images-32x32.npy")
+        # caption data, n * 57 (max length of caption)
+        captions_path = Path(datadir, f"{split}-captions.npy")
+        # length of captions # n * 1
+        captions_len = Path(datadir, f"{split}-captions-len.npy")
+        # mapping from caption index to image index # (essentially just i//5)
+        cap2im = Path(datadir, f"{split}-cap2im.pkl")
+
+        self.data = np.load(data_path).astype(np.float32)
+        self.captions = np.load(captions_path).astype(np.int64)
+        self.captions_len = np.load(captions_len).astype(np.int32).reshape(-1)
+        with open(cap2im, "rb") as f:
+            self.cap2im = pickle.load(f)
+
+    def __len__(self):
+        return self.captions.shape[0]
+
+    def __getitem__(self, index):
+        # by using the custom batch sampler defined below, all captions within a batch have the same length,
+        # so we don't need to use collate_fn to pad shorter captions
+        return self.data[self.cap2im[index]], self.captions[index][0 : self.captions_len[index]]
+
+
+# custom batch sampler to make sure captions within a batch have the same length
+# the logic is identical to homogeneous-data.py in mansimov's code
+class CaptionSameLenBatchSampler(Sampler):
+    def __init__(self, dataset, batch_size, seed=5, minlen=7, maxlen=30) -> None:
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.seed = seed
+
+        captions_len = dataset.captions_len
+        self.len_to_indices = defaultdict(list)
+        self.rng = np.random.default_rng(seed)
+
+        for i, l in enumerate(captions_len):
+            if (minlen is None or l >= minlen) and (maxlen is None or l <= maxlen):
+                self.len_to_indices[l].append(i)
+        # have to compute the length after filtering by minlen and maxlen
+        self.size = 0
+        for l in self.len_to_indices:
+            self.size += math.ceil(len(self.len_to_indices[l]) / self.batch_size)
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        batches = []
+
+        for l in self.len_to_indices:
+            self.rng.shuffle(self.len_to_indices[l])
+            # torch.split doesn't work the same as np.split
+            for batch in torch.split(torch.tensor(self.len_to_indices[l]), self.batch_size):
+                batches.append(batch.tolist())
+        self.rng.shuffle(batches)
+
+        return iter(batches)
 
 
 class MNISTCaptions(Dataset):
@@ -37,7 +107,7 @@ class MNISTCaptions(Dataset):
             ".": 21,
         }
         self.reverse_dictionary = create_reverse_dictionary(self.dictionary)
-        
+
         # may use transforms.ToTensor to convert PIL to 0-1 tensor
         mnist = datasets.MNIST(datadir, train=train, download=True)
         self.data = mnist.data.numpy() / 255  # 28 * 28, 0 - 255
@@ -46,20 +116,20 @@ class MNISTCaptions(Dataset):
 
     def __len__(self):
         return self.size
-    
+
     def __getitem__(self, index):
         while True:
             k = randint(0, 7)
             i = randint(0, self.size)
             j = randint(0, self.size)
-            
+
             image = self.getImage(k, i, j)
             caption = self.getCaption(k, i, j)
             if caption is None:
                 continue
-            
+
             return image, caption
-        
+
     def getImage(self, k, i, j):
         if k == 0 or k == 1:
             image = create_2digit_mnist_image_leftright(self.data[i], self.data[j])
@@ -73,80 +143,57 @@ class MNISTCaptions(Dataset):
             image = create_1digit_mnist_image_topright(self.data[i])
         if k == 7:
             image = create_1digit_mnist_image_bottomleft(self.data[i])
-            
+
         return image
-    
+
     def getCaption(self, k, i, j):
         # some cases are hidden from training set
         if k <= 3:
-            if (
-                self.targets[i] == self.banned[k * 2]
-                or self.targets[j] == self.banned[k * 2 + 1]
-            ):
+            if self.targets[i] == self.banned[k * 2] or self.targets[j] == self.banned[k * 2 + 1]:
                 return None
         else:
             if self.targets[i] == self.banned[k + 4]:
                 return None
 
         if k == 0:
-            sentence = "the digit %d is on the left of the digit %d ." % (
-                self.targets[i],
-                self.targets[j],
-            )
+            sentence = "the digit %d is on the left of the digit %d ." % (self.targets[i], self.targets[j])
         elif k == 1:
-            sentence = "the digit %d is on the right of the digit %d ." % (
-                self.targets[j],
-                self.targets[i],
-            )
+            sentence = "the digit %d is on the right of the digit %d ." % (self.targets[j], self.targets[i])
         elif k == 2:
-            sentence = "the digit %d is at the top of the digit %d ." % (
-                self.targets[i],
-                self.targets[j],
-            )
+            sentence = "the digit %d is at the top of the digit %d ." % (self.targets[i], self.targets[j])
         elif k == 3:
-            sentence = "the digit %d is at the bottom of the digit %d ." % (
-                self.targets[j],
-                self.targets[i],
-            )
+            sentence = "the digit %d is at the bottom of the digit %d ." % (self.targets[j], self.targets[i])
         elif k == 4:
-            sentence = "the digit %d is at the top left of the image ." % (
-                self.targets[i]
-            )
+            sentence = "the digit %d is at the top left of the image ." % (self.targets[i])
         elif k == 5:
-            sentence = "the digit %d is at the bottom right of the image ." % (
-                self.targets[i]
-            )
+            sentence = "the digit %d is at the bottom right of the image ." % (self.targets[i])
         elif k == 6:
-            sentence = "the digit %d is at the top right of the image ." % (
-                self.targets[i]
-            )
+            sentence = "the digit %d is at the top right of the image ." % (self.targets[i])
         elif k == 7:
-            sentence = "the digit %d is at the bottom left of the image ." % (
-                self.targets[i]
-            )
+            sentence = "the digit %d is at the bottom left of the image ." % (self.targets[i])
 
         caption = sent2matrix(sentence, self.dictionary)
-        
+
         return caption
 
 
 class Captions(MNISTCaptions):
     def __init__(self, datadir, banned, size=64, train=True):
         super().__init__(datadir, banned, size, train)
-    
+
     def __getitem__(self, index):
         while True:
             k = randint(0, 7)
             i = randint(0, self.size)
             j = randint(0, self.size)
-            
+
             caption = self.getCaption(k, i, j)
             if caption is None:
                 continue
-            
+
             return caption
-    
-    
+
+
 def create_reverse_dictionary(dictionary):
     dictionary_reverse = {}
 
@@ -158,9 +205,7 @@ def create_reverse_dictionary(dictionary):
 
 def sent2matrix(sentence, dictionary):
     words = sentence.split()
-    m = np.zeros(
-        len(words), dtype=np.int64
-    )  # int64 required for using torch.nn.functional.on_hot
+    m = np.zeros(len(words), dtype=np.int64)  # int64 required for using torch.nn.functional.on_hot
 
     for i in range(len(words)):
         m[i] = dictionary[words[i]]
@@ -265,11 +310,36 @@ def create_1digit_mnist_image_bottomleft(digit1):
 
     return image
 
-if __name__ == "__main__":
-    cap = datasets.CocoCaptions(root = "data/coco/train2014/",
-                        annFile = "data/coco/annotations/captions_train2014.json",
-                        transform=transforms.PILToTensor())
-    img, target = cap[0]
 
-    print("Image Size: ", img.size())
-    print(target)
+if __name__ == "__main__":
+    # cap = datasets.CocoCaptions(root = "data/coco/train2014/",
+    #                     annFile = "data/coco/annotations/captions_train2014.json",
+    #                     transform=transforms.PILToTensor())
+    # img, target = cap[0]
+
+    # print("Image Size: ", img.size())
+    # print(target)
+
+    def towords(s, reverse_dict):
+        return " ".join([reverse_dict[e] for e in s if e > 0])
+
+    data = np.float32(np.load("data/train-images-32x32.npy"))
+    data_captions = np.int32(np.load("data/train-captions.npy"))
+    data_captions_len = np.int32(np.load("data/train-captions-len.npy"))
+    with open("data/train-cap2im.pkl", "rb") as f:
+        data_cap2im = pickle.load(f)
+
+    print(data.shape)  # n * 3072
+    print(data_captions.shape)  # (5*n) * max(seq)
+    print(data_captions_len.shape)  # (5*n) * 1
+    for key in data_cap2im:
+        assert data_cap2im[key] == key // 5
+    # print(data_cap2im[5])
+
+    with open("data/dictionary.pkl", "rb") as f:
+        dictionary = pickle.load(f)
+    print(len(list(dictionary.keys())))
+    reverse_dictionary = create_reverse_dictionary(dictionary)
+
+    for i in range(10):
+        print(towords(data_captions[i], reverse_dictionary))
