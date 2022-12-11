354243aa2ffff5f7ca7b6607a02ab1bc55230cd7
Bowen Li <bowenli@perceptron.cm.cluster>
2022-12-11 14:48:13 -0500

    merged change

--- src/model_builder.py
+++ src/model_builder.py
@@ -1,8 +1,10 @@
 from aligndraw import AlignDraw # basemodels
 from clipdraw import AlignDrawClip # basemodels
+from clipdrawcat import AlignDrawClipCat # basemodels
 
 
 BUILDER = {
     'AlignDraw': AlignDraw,
-    'AlignDrawClip': AlignDrawClip # add
+    'AlignDrawClip': AlignDrawClip, # add
+    'AlignDrawClipCat': AlignDrawClipCat # cat
 }
